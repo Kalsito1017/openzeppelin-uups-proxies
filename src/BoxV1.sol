bd956bfb42fb3f4dc8c79d32752b307e2c32b9ee
Kaloyan Stoichkov <kaloyanstoichkov9@gmail.com>
2025-08-02 14:03:08 +0300

    Added files and folders

---
+++ src/BoxV1.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract BoxV1 {
+    uint256 internal number;
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+}
